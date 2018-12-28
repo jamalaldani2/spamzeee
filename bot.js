bcc7893afaa9af51c9e62eabd60aaf35a0c30366
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-28 13:51:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-var prefix = "W";
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -11,8 +8,8 @@ client.on('ready', () => {
 
 
 client.on('ready', async() => {
-var server = "515202282824925184"; // ايدي السررفر
-var channel = "515206756201136130";//ايدي الروم
+var server = "Id server"; // ايدي السررفر
+var channel = "id room";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , Forest Spam , ')
     },305);
